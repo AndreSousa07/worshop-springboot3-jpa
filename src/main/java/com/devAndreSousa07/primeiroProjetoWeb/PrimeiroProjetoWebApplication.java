4f3f032d890fc276b379efb8164ed5c61a92a3de
Andre Sousa07 <andregomessousa05@gmail.com>
2023-10-21 19:12:24 -0300

    Project created

---
+++ src/main/java/com/devAndreSousa07/primeiroProjetoWeb/PrimeiroProjetoWebApplication.java
@@ -0,0 +1,13 @@
+package com.devAndreSousa07.primeiroProjetoWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoWebApplication.class, args);
+	}
+
+}
